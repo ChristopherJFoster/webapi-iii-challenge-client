9fb5b6a4ec2700e930f9d1c5e1adc9eccba6bffa
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-21 19:26:15 -0400

    Added conditional rendering to Post.js because it was throwing an error. I think the error comes from there being no posts to display due to a server delay.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 export const Post = ({ post, allUsers }) => {
-  //   let user = '';
-
-  //   useEffect(() => {
-  //     if (!user) {
-  //       user = allUsers.filter(user => user.id === post.user_id)[0].name;
-  //     }
-  //   }, []);
-
   return (
-    <div className='post'>
-      <h4>{allUsers.filter(user => user.id === post.user_id)[0].name}</h4>
-      <p>{post.text}</p>
+    <div>
+      {allUsers.length > 0 && (
+        <div className='post'>
+          <h4>{allUsers.filter(user => user.id === post.user_id)[0].name}</h4>
+          <p>{post.text}</p>
+        </div>
+      )}
     </div>
   );
 };
